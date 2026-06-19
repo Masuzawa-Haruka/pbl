61e7cec052bdadc5f3b492a1ff217eec809461af
Masuzawa-Haruka <haru.keyaki.mas@gmail.com>
2026-06-19 15:10:36 +0900

    修正

--- trading_text/main/urls.py
+++ trading_text/main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+    path('listing/new/', views.listing_form, name='listing_form'),
+    path('inbox/', views.inbox, name='inbox'),
+    path('mypage/', views.mypage, name='mypage'),
 ]
