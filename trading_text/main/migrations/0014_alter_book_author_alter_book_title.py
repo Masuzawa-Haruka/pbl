ab892a81b8f7e0f5c225622ec87cd37711e2bfbe
Masuzawa-Haruka <haru.keyaki.mas@gmail.com>
2026-07-24 02:40:14 +0900

    マイグレート修正

--- trading_text/main/migrations/0014_alter_book_author_alter_book_title.py
+++ trading_text/main/migrations/0014_alter_book_author_alter_book_title.py
@@ -1,12 +1,30 @@
 from django.db import migrations, models
 
 
+def truncate_existing_book_text(apps, schema_editor):
+    Book = apps.get_model("main", "Book")
+    database_alias = schema_editor.connection.alias
+
+    for book in Book.objects.using(database_alias).only("id", "title", "author").iterator():
+        truncated_title = book.title[:60]
+        truncated_author = book.author[:40]
+        if truncated_title != book.title or truncated_author != book.author:
+            Book.objects.using(database_alias).filter(pk=book.pk).update(
+                title=truncated_title,
+                author=truncated_author,
+            )
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("main", "0013_update_book_filter_choices"),
     ]
 
     operations = [
+        migrations.RunPython(
+            truncate_existing_book_text,
+            reverse_code=migrations.RunPython.noop,
+        ),
         migrations.AlterField(
             model_name="book",
             name="author",
